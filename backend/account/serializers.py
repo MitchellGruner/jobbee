ead8e7c52dfbba7922e3c01bc2af33a198f97e45
Mitchell Gruner <mit.gruner@gmail.com>
2024-08-20 18:40:24 -0500

    'require' to 'required'

--- backend/account/serializers.py
+++ backend/account/serializers.py
@@ -6,10 +6,10 @@ class SignUpSerializer(serializers.ModelSerializer):
         model = User
         fields = ['first_name', 'last_name', 'email', 'password']
         extra_kwargs = {
-            'first_name': {'require': True, 'allow_blank': False},
-            'last_name': {'require': True, 'allow_blank': False},
-            'email': {'require': True, 'allow_blank': False},
-            'password': {'require': True, 'allow_blank': False, 'min_length': 6},
+            'first_name': {'required': True, 'allow_blank': False},
+            'last_name': {'required': True, 'allow_blank': False},
+            'email': {'required': True, 'allow_blank': False},
+            'password': {'required': True, 'allow_blank': False, 'min_length': 6},
         }
     
     def create(self, validated_data):
